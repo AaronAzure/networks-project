fc7e4a9bf5b26033f989eb39513f8cd93f5bcb29
Xiang <69409366+DLing13@users.noreply.github.com>
2022-04-29 23:02:29 +0800

    Python client file generated.

---
+++ daniel_testing/client-p.py
@@ -0,0 +1,56 @@
+import os
+
+# Function that receives a filename and then returns a list of lines in that file.
+def fread(filename):
+	rfile = open(filename, 'r')
+	
+	lines = rfile.readlines()
+	
+	rfile.close()
+	
+	count = 0
+	
+	for line in lines:
+		
+		if line.find('#') >= 0:	# strip '#' and all characters after it from that line
+		
+			split_string = line.split("#", 1)
+			lines[count] = split_string[0]
+		
+		lines[count] = lines[count].strip()	# strip newline
+		count += 1
+	return lines
+
+# Function that converts all items in a list to a dictionary, which is then returned.	
+def dict_process(items):
+	
+	# Dictionary holding the port number, a 1D array of hosts and a 2D array of action sets.
+	item_dictionary = {'Port': '', 'Hosts': [], 'Action Sets': []}
+	
+	# variables for the action set
+	a_set_count = -1
+	a_set = []
+	
+	for item in items:
+		if item.find('PORT') >= 0:
+			item_dictionary['Port'] = item.split("= ", 1)[1]
+		
+		if item.find('HOST') >= 0:
+			item = item.replace('HOSTS = ', '')
+			item_dictionary['Hosts'] = item.split(' ', item.count(' '))
+	
+	return item_dictionary
+
+def main():
+	arr = os.listdir()	# array of all files in the client folder.
+	for item in arr:
+		if not item == 'client-p.py' and not os.stat(item).st_size == 0:
+			s_list = fread(item)
+			rake_dict = dict_process(s_list)
+			print('\n', 'This is what was in the file\n', s_list)
+			print('\n', 'This is a dictionary of the port, hosts and action sets\n',rake_dict)
+	
+main()
+
+# NOTE: SHOULD WORK WITH `python3 client-p.py`
+# ASSUMPTION: USING PYTHON3
