8d70a9383f68029c20e077123de979a744aa0b53
AaronAzure <azurakeni@gmail.com>
2022-05-06 13:43:07 +0800

    imported socket module
    binds socket and waits for clients

--- rakeserver.py
+++ rakeserver.py
@@ -1,22 +1,38 @@
-import sys
+import socket
 
 # port number
 port_num = 12345
 
-# total arguments
-n_args = len(sys.argv)
-# print(n_args)
-
-# Using argparse module
-if n_args > 1:
-    port_num = int(sys.argv[1])
-
 
 def main():
-    print("listening on port=" + str(port_num) + ", sd=")
+    local_ip = socket.gethostbyname("localhost")
+
+    # A TCP based echo server
+    echo_socket = socket.socket()
+ 
+    # Bind the IP address and the port number
+    echo_socket.bind((str(local_ip), port_num))
+
+    print("IP address = " + str(local_ip))
+    print("listening on port=" + str(port_num) + ", sd=" + "\n")
+
+    # Listen for incoming connections
+    echo_socket.listen()
+
+    # Start accepting client connections
+    while(True):
+        (clientSocket, clientAddress) = echo_socket.accept()
+
+        # Handle one request from client
+        while(True):
+            data = clientSocket.recv(1024)
+            print("At Server: %s"%data)
+
+            if(data!=b''):
+                # Send back what you received
+                clientSocket.send(data)
+                break
+
     
 
 main()
-
-
-
