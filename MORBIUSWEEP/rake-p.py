5c33a0feb3fde09d126af679473ff2dd16843cd1
Xiang <69409366+DLing13@users.noreply.github.com>
2022-05-24 13:21:19 +0800

    Cleanup on client, server. Included test rakefile

---
+++ MORBIUSWEEP/rake-p.py
@@ -0,0 +1,400 @@
+# CITS3002 2022 Project, written by:
+# Muhammad Maaz Ahmed	(22436686)
+# Aaron Wee				(22702446)
+# Daniel Ling			(22896002)
+
+import select, shutil, socket, struct, subprocess, sys
+import getopt, os
+import time #! DELETE
+
+BOLD = "\033[1;30m"
+RED = "\033[1;31m"
+BLU = "\033[1;34m"
+GRN = "\033[1;32m"
+YEL = "\033[1;33m"
+MAG = "\033[1;35m"
+CYN = "\033[1;36m"
+RST = "\033[0m"
+
+############ HARDCODE, GET RID OF THIS LATER ##################################
+HOST = 'localhost'
+PORT_NUM = 12345
+DEFAULT_PORT = 12345
+VERBOSE = False
+rakefile  = 'Rakefile'	# Will be used to store rakefile.
+#################################################################################
+
+def fread(filename):
+	'''
+	Function that receives a filename and then returns important, stripped list of lines.
+	'''
+	rfile = open(filename, 'r')
+	lines = rfile.readlines()
+	rfile.close()
+	
+	count = 0
+	result = []
+	for line in lines:
+
+		# skip empty lines
+		if line == '\n' or line == '':
+			continue
+
+		# strip '#' and all characters after it from that line
+		if line.find('#') == 0:
+			continue
+		elif line.find('#') > 0:
+			result.append(line.split('#', 1)[0])
+		else:
+			result.append(line)
+		
+		result[count] = result[count].rstrip('\n')	# strip newline
+		result[count] = result[count].rstrip()		# strip trailing whitespaces
+		count += 1
+	return result
+
+
+def extract_info(items):
+	'''
+	Extract important information from @param items into a dictionary (mapping) format.	
+	'''
+	# Dictionary holding the port number, a 1D array of hosts and a 2D arrays of action sets.
+	item_dictionary = {'Port': '', 'Hosts': []}
+	
+	# variables for the action set
+	count = 1
+	current_actionset = ''
+	
+	global DEFAULT_PORT
+	for item in items:
+		# LINE STARTS WITH PORT, SO GET AND STORE DEFAULT PORT NUMBER.
+		if item.find('PORT') >= 0:
+			DEFAULT_PORT = int(item.split('= ', 1)[1])
+			item_dictionary['Port'] = DEFAULT_PORT
+		
+		# LINE STARTS WITH HOSTS, SO GET AND STORE HOST(S).
+		elif item.find('HOSTS') >= 0:
+			item = item.replace('HOSTS = ', '')
+			item_dictionary['Hosts'] = item.split(' ', item.count(' '))
+			
+		# LINE STARTS WITH actionset, GET AND STORE Actionset.
+		elif item.find('actionset' + str(count) + ':') >= 0:
+			current_actionset = 'Action Set ' + str(count)
+			item_dictionary[ current_actionset ] = []
+			count += 1
+		
+		# STORE ACTION.
+		elif item.count('\t') == 1 and item != '\t':
+			action = item.strip()
+			if len(action) > 0:
+				item_dictionary[ current_actionset ].append( [action] )
+		
+		# STORE REQUIREMENTS FOR PREVIOUS ACTION.
+		elif item.count('\t') == 2 and len(item_dictionary[ current_actionset ]) > 0:
+			req_file = item.strip()
+			if len(req_file) > 0:
+				item_dictionary[ current_actionset ][-1].append( req_file )
+		
+	# All hosts with no specified port number get assigned the default port number.
+	count = 0
+	for host in item_dictionary.get('Hosts'):
+		if host.find(':') >= 0:
+			item_dictionary['Hosts'][count] = (host.split(':', 1)[0], int(host.split(':', 1)[1]))
+		elif host == '':
+			item_dictionary['Hosts'].remove(host)
+		else:
+			item_dictionary['Hosts'][count] = (host, item_dictionary['Port'])
+		count += 1
+			
+	return item_dictionary
+
+
+def get_action_set_names(rdictionary):
+	'''
+	Return actionsets headers.
+	'''
+	actionsets = []			# ACTIONSET KEYS LIST.
+	action_failure = False		# IF TRUE, AN ACTION HAS FAILED. DO NOT EXECUTE NEXT ACTIONSET.
+	
+	for key in rdictionary:
+		if key.find('Action Set ') >= 0:
+			actionsets.append(key)
+
+	return actionsets
+
+
+def execute_on_server(server_port_tuple, argument, requirements=[]):
+	'''
+	Function that performes actions that require the server. 
+	Receives the action and a list of the necessary files.
+	'''
+	
+	sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sd.connect( server_port_tuple )
+	
+	header = struct.pack('i i i', 0, len(argument), len(requirements))
+	sd.send( header )
+	
+	# SENDS SERVER THE COMMAND/ACTION TO BE EXECUTED
+	sd.send(bytes(argument, "utf-8"))
+
+	# SEND FILES TO SERVER
+	if requirements != None:
+		for requirement in requirements:
+			print(f'{BLU}> sending', requirement, RST)
+
+			# INFORM THE SERVER THE LENGTH OF THE FILE NAME AND SIZE OF THE FILE TO RECEIVE
+			file_size = os.path.getsize( requirement )
+			file_name_length = len(requirement)
+			file_struct = struct.pack('i i i', 0, file_name_length, file_size)
+
+			print(f'{BLU}> filesize = {file_size}, file name length = {file_name_length}, sent as {file_struct}', RST)
+			sd.send( file_struct )
+			
+			file = open(requirement, 'rb')
+			file_to_send = file.read()
+			sd.send( file_to_send )
+
+			sd.send( bytes(requirement, 'utf-8') )				# SENDING FILE NAME.
+			
+			file.close()
+
+	return sd
+	
+
+def get_cheapest_host(hosts, argument, requirements=None):
+	'''
+	Simultaneously get the cost of each remote host, 
+	and report back the remote host with the lowest cost
+	'''
+	if requirements != None:
+		argument += ' Requirements: '
+		for requirement in requirements:
+			argument += requirement
+			argument += ' '
+	
+	frame = struct.pack('i i i', 1, len(argument), 0)
+
+	cheapest_host = 0
+	lowest_cost = float('inf')
+
+	for i in range(len(hosts)):
+		sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sd.connect( hosts[i] )
+
+		sd.send( frame )
+
+		# RECEIVE THE COST FOR THE COMMAND
+		reply = sd.recv(4)
+		reply = struct.unpack('i', reply)
+		cost = reply[0]
+		
+		# REMEMBER THE REMOTE HOST RETURNING THE LOWEST COST TO RUN THE COMMAND
+		if cost < lowest_cost:
+			lowest_cost = cost
+			cheapest_host = i
+
+	return cheapest_host
+	
+
+def read_option_flags():
+	global HOST
+	global VERBOSE
+	global PORT_NUM
+	global SOCKET_NUM
+	global rakefile 
+
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "vhi:p:r:")
+		
+		for opt, arg in opts:
+			# HELP ( HOW TO USE )
+			if opt == '-h':
+				print('usage: rake-p.py -i <ip address> -p <port number> -r <rakefile>')
+				sys.exit()
+			# IP ADDRESS
+			elif opt == '-i':
+				HOST = arg
+			# RAKEFILE TO ANAYLSE
+			elif opt == '-r':
+				rakefile = arg
+			# PORT NUMBER
+			elif opt == "-p":
+				PORT_NUM = int(arg)
+			# VERBOSE - DEBUGGING
+			elif opt == "-v":
+				VERBOSE = True
+	except getopt.GetoptError:
+		print('usage: rakeserver.py -i <ip address> -p <port number> -r <rakefile>')
+		sys.exit(2)
+
+def parse_server_frame (mad_frame, sd):
+	'''
+	Given the MAD frame from the server and the socket descriptor, client
+	prepares to receive and indicate the exit status, stdout, stderr as well as
+	any output file. If exit status != 0, then return True to indicate an 
+	error has occured. False otherwise.
+	'''
+	frame_data = struct.unpack('i i i i i', mad_frame)
+	exit_status 			= frame_data[0]
+	output_len 				= frame_data[1]
+	output_filesize 		= frame_data[2]
+	output_filename_length 	= frame_data[3]
+	error_len 				= frame_data[4]
+		
+	print(f"< status:\n{exit_status}")
+
+	if output_len > 0:
+		output = sd.recv( output_len ).decode("utf-8")
+		print("< stdout:")
+		print(output, file=sys.stdout)
+	else:
+		print("< stdout:")
+		print("", file=sys.stdout)
+
+	if error_len > 0:
+		err = sd.recv( error_len ).decode("utf-8")
+		print("< stderr:")
+		print(err, file=sys.stderr)
+	else:
+		print("< stderr:")
+		print("", file=sys.stderr)
+
+	# RECEIVING OUTPUT FILE
+	if output_filesize > 0 and output_filename_length > 0:			
+		output_file = sd.recv( output_filesize )
+		output_filename = sd.recv( output_filename_length ).decode("utf-8")
+
+		file = open(output_filename, 'wb')
+		file.write(output_file)
+		file.close()
+		
+		print(f"< output file name:\n{output_filename}")
+	
+	if exit_status != 0:
+		return True
+	return False
+
+####################################################################################################
+
+def main():
+	global HOST
+	global PORT_NUM
+	global DEFAULT_PORT
+	global VERBOSE
+	global rakefile 
+
+	# OPTION FLAGS
+	read_option_flags()
+	
+	if VERBOSE:
+		print('\n', 'Looking at this file:', rakefile)
+		
+	string_list = fread(rakefile)
+	
+	###DEBUG###
+	if VERBOSE:
+		print('\nThis is what was in the file:\n', string_list, '\n')
+
+	rake_dict = extract_info(string_list)
+	
+	###DEBUG###
+	if VERBOSE:
+		print('\nDictionary:','\n', rake_dict, '\n')
+	
+	hosts = rake_dict['Hosts']
+	actionset_names = get_action_set_names(rake_dict)
+
+	if VERBOSE:
+		print(f"hosts = {hosts}")
+		print(f"actionset_names = {actionset_names}")
+		start_time = time.time()
+	
+
+	# PERFORM ACTION ON SERVER ( HOSTS )
+	error_in_actionset = False
+	for actionset in actionset_names:
+
+		current_action = 0
+		outputs_received = 0
+		total_actions = len(rake_dict[ actionset ])
+		still_waiting_for_outputs = True
+
+		inputs = []
+
+		if VERBOSE:
+			print(GRN, "--------------------------------------------------------------------------------", RST)
+			print('starting', actionset)
+			print(f"{GRN}{rake_dict[actionset]}{RST}")
+
+		while still_waiting_for_outputs:
+			if current_action < total_actions:
+				action = rake_dict[actionset][current_action]
+
+				if VERBOSE:
+					print(f'{CYN}running {action[0]}{RST}')
+				
+				# CHECK IF ACTION HAS REQUIREMENTS
+				requirements = []
+				if len(action) > 1: # HAS REQUIREMENT FILES
+					requirements = action[1].split()[1:]
+
+				#* IF ACTION IS REMOTE, THEN CHECK COST FROM EACH REMOTE SERVER
+				if action[0].find('remote-') == 0:
+					argument = action[0].split("remote-")[1]
+
+					cheapest_host = get_cheapest_host( hosts, argument, requirements) # cost simulateonusly
+
+					# EXECUTE ON CHEAPEST REMOTE HOST
+					print(f"{YEL} --- REMOTE EXECUTION --- {RST}")
+					sd = execute_on_server( hosts[cheapest_host] , argument , requirements )
+					print(f"{BLU}> {argument} {requirements}{RST}")
+					inputs.append(sd)
+			
+				#* IF ACTION IS LOCAL, EXECUTE ON LOCAL SERVER
+				else:
+					print(f"{YEL} --- LOCAL EXECUTION --- {RST}")
+					sd = execute_on_server( ( 'localhost' , DEFAULT_PORT ) , action[0], requirements )
+					print(f"{BLU}> {action[0]} {requirements}{RST}")
+					inputs.append(sd)
+
+				current_action += 1
+			
+			if inputs:
+				readable, writable, exceptional = select.select(inputs, [], inputs, 0)
+				# READ FROM SERVER, WHILST CONNECTED
+				for sd in readable:
+					print(MAG, "----------------------------------------", RST)
+					print(MAG, f"from {sd.getpeername()}", RST)
+					
+					# SERVER TO CLIENT FRAME: EXIT STATUS, OUTPUT LENGTH, OUTPUT FILE SIZE
+					#                         OUTPUT FILE NAME LENGTH, ERROR LENGTH
+					mad_frame_server = sd.recv(20)
+					if mad_frame_server:
+						error_in_actionset = parse_server_frame(mad_frame_server, sd)
+					
+					sd.close()
+					inputs.remove(sd)
+					
+					outputs_received += 1
+					if outputs_received >= total_actions:
+						still_waiting_for_outputs = False
+					
+				for sd in exceptional:
+					error_in_actionset = True
+					sd.close()
+					inputs.remove(sd)
+
+		if error_in_actionset:
+			print(f"{RED}error detected in actionset - halting subsequent actionsets{RST}")
+			break
+
+		time.sleep(1)
+		
+	if VERBOSE:
+		print(f"{YEL}----------------------------------{RST}")
+		print(f"{YEL}  EXECUTION TIME = {(time.time() - start_time):.2f}s{RST}")
+		print(f"{YEL}----------------------------------{RST}")
+		
+if __name__ == "__main__":
+	main()
---
+++ MORBIUSWEEP/rakeserver.py
@@ -0,0 +1,287 @@
+# CITS3003 2022 Project, written by:
+# Muhammad Maaz Ahmed	(22436686)
+# Aaron Wee				(22702446)
+# Daniel Ling			(22896002)
+
+import shutil, socket, struct, subprocess, sys
+import getopt, os, random, tempfile
+import time #! DELETE
+
+# port number
+HOST = 'localhost'
+PORT_NUM = 12345
+SOCKET_NUM = 0
+VERBOSE = False
+
+BOLD = "\033[1;30m"
+RED = "\033[1;31m"
+GRN = "\033[1;32m"
+YEL = "\033[1;33m"
+BLU = "\033[1;34m"
+MAG = "\033[1;35m"
+CYN = "\033[1;36m"
+RST = "\033[0m"
+
+def send_cost(client):
+	'''
+	Sends the cost of executing an action on this server to the client.
+	'''
+	quote = random.randint(1, 100)
+	cost_response = struct.pack('i', quote)
+	print("> cost =", quote)
+	client.send( cost_response )
+	client.close()
+
+def find_output_file(directory, latest_update):
+	''' 
+	Function finds the output file in the temporary directory. @param directory is the 
+	working directory. latest_update is the latest modification time of any files in 
+	the working directory. @returns the file whose modification time exceeds latest_update.
+	None otherwise.
+	''' 
+	
+	if latest_update == 0.0:	# NO FILE IN TEMPORARY DIRECTORY.
+		return None
+		
+	all_files = [f for f in os.listdir( directory ) if os.path.isfile(os.path.join(directory, f))]
+	
+	for f in all_files:
+		if os.path.getmtime(os.path.join(directory, f)) > latest_update:
+			return f
+	return None
+
+def get_req_files(requirement_length, client, latest_update):
+	'''
+	Server prepares to receive files and write them into the temporary directory.  
+	@param requirement_length is the number of files required for the action, 
+	client is client communicating witht he server, latest_update is the latest 
+	modification time of any files in the working directory. @returns the latest
+	modification time of the last modified file in the working directory.
+	'''
+
+	# RECEIVE EACH REQUIRED FILE
+	for i in range(requirement_length):
+	# for required_file in requirements:
+		req_file_data = client.recv(12)     #! BLOCKING
+
+		# DECRYPT HEADER
+		if req_file_data:
+			req_file_header = struct.unpack('i i i', req_file_data)
+			print(f"{YEL}{req_file_header}{RST}")
+			# asking_for_cost = bool(header[0])
+			filename_length = req_file_header[1]
+			file_to_recv_size = req_file_header[2]
+
+			file_data = client.recv( file_to_recv_size )
+			
+			# GET THE NAME OF THE FILE TO BE RECEIVED FROM THE CLIENT
+			filename = client.recv( filename_length ).decode("utf-8")
+			print(f"--|{filename}|--")
+			
+			print(f"creating file {filename}")
+			file = open(filename, "wb")
+			file.write( file_data )
+			file.close()
+		
+			# STORES MODIFIED TIME OF FILE IF IT EXCEEDS latest_update.
+			if latest_update < os.path.getmtime( filename ):
+				latest_update = os.path.getmtime( filename )
+	
+	return latest_update
+
+def send_output(execution, client, temp_dir, output_file):
+	'''
+	Multi-step process to send output back to the client. First sends a frame header. 
+	Then sends output, error or file and file name (if any exists). @param execution is 
+	the executed action, client is the client communicating with the server, temp_dir is 
+	the directory where any files for the current action are located, output_file is the 
+	file to be sent back to the client, if it exists. @returns exit status of execution.
+	'''
+	# PREPARING TO SEND SERVER TO CLIENT HEADER.
+	exit_status = execution.returncode			# OUTPUT RETURN CODE.
+	output = execution.stdout.decode("utf-8")	# OUTPUT.
+	filesize = 0								# OUTPUT FILE SIZE.
+	filename_length = 0							# OUTPUT FILE NAME LENGTH.
+	err = execution.stderr.decode("utf-8")		# OUTPUT ERROR MESSAGE.
+	
+	if output_file != None:			# OUTPUT FILE EXISTS.
+		filesize = os.path.getsize(output_file)
+		filename_length = len(output_file)
+
+	header = struct.pack('i i i i i', exit_status, len(output), filesize, filename_length, len(err))
+	print(f"> stat={execution.returncode}, output len={len(output)}, file size={filesize}, file name length={filename_length}, err len={len(err)}")
+	client.send(header)
+
+	print(f"> out={output}")
+	if len(output) > 0:
+		client.send(bytes(output, "utf-8"))
+		
+	print(f"> err={err}")
+	if len(err) > 0:
+		client.send(bytes(err, "utf-8"))
+
+	if output_file != None:			# OUTPUT FILE EXISTS.
+		
+		file = open(output_file, 'rb')
+		reading_file = file.read()
+			
+		client.send(reading_file)
+		
+		print(f"> file={output_file}")
+		client.send(bytes(output_file, "utf-8"))
+		
+		file.close()
+		
+		if VERBOSE:
+			input_files = [f for f in os.listdir( temp_dir ) if os.path.isfile( f )]
+			input_files.remove( output_file )
+			print(f"{RED} output file = {output_file}",RST)
+			print(f"{GRN} input files = {input_files}",RST)
+
+	return exit_status
+
+def read_option_flags():
+	'''
+	Function that reads option flags at the command line.
+	'''
+	
+	global HOST
+	global VERBOSE
+	global PORT_NUM
+	global SOCKET_NUM
+	
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "vhi:p:")
+		
+		for opt, arg in opts:
+			# HELP ( HOW TO USE )
+			if opt == '-h':
+				print('usage: rakeserver.py -p <port number>')
+				sys.exit()
+			# IP ADDRESS
+			elif opt == '-i':
+				HOST = arg
+			# PORT NUMBER
+			elif opt == "-p":
+				PORT_NUM = int(arg)
+			# VERBOSE - DEBUGGING
+			elif opt == "-v":
+				VERBOSE = True
+	except getopt.GetoptError:
+		print('usage: rakeserver.py -i <ip address> -p <port number>')
+		sys.exit(2)
+
+def main():
+	global HOST
+	global VERBOSE
+	global PORT_NUM
+	global SOCKET_NUM
+
+	read_option_flags()
+
+	# A TCP-based server
+	sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	# Bind the IP address and the port number
+	sd.bind((HOST, PORT_NUM))
+	
+	SOCKET_NUM = 0
+	# local_ip = socket.gethostbyname("localhost")
+	if VERBOSE:
+		print("IP address = " + HOST)
+	print(MAG + "listening on port=" + str(PORT_NUM) + ", sd=" + str(SOCKET_NUM) + RST)
+	print("---------------------------------------------")
+
+	# Listen for incoming connections
+	sd.listen()
+
+	# Start accepting client connections
+	while True:
+		client, addr = sd.accept() #! BLOCKING
+		SOCKET_NUM += 1
+		print(BOLD + " Accepted new client on sd=" + str(SOCKET_NUM) + RST)
+		
+		while True: 
+			data = client.recv(12)     #! BLOCKING
+			if data != None:
+				# DECRYPT HEADER
+				header = struct.unpack('i i i', data)
+				asking_for_cost 	= bool(header[0])
+				command_length 		= header[1]
+				requirement_length 	= header[2]
+
+				# CLIENT ASKING FOR QUOTE/COST FOR EXECUTING COMMAND
+				if asking_for_cost:
+					send_cost(client)
+					data = None
+					break
+
+				pid = os.fork()
+
+				# CHILD PROCESS DEALS WITH CURRENT ACTION
+				if pid == 0:
+					time.sleep(os.getpid() % 5 * 0.2)
+
+					argument = client.recv( command_length ).decode("utf-8")	#! BLOCKING
+				
+					print(f"< {header}")
+					latest_update = 0.0		# STORES TIME OF LATEST UPDATED FILE.
+
+					if VERBOSE:
+						print(f'{GRN}< argument:', argument, RST)
+					
+					# SERVER DIRECTORY
+					server_dir = os.getcwd()	# SERVER DIRECTORY	
+					temp_dir = tempfile.mkdtemp()	# TEMPORARY DIRECTORY FOR ALL FILES.
+					os.chdir(temp_dir)
+
+					# THERE ARE REQUIREMENTS
+					if requirement_length > 0:
+						print(f"{BLU} - TEMPORARY DIRECTORY: {temp_dir} - {RST}")
+						latest_update = get_req_files(requirement_length, client, latest_update)
+
+					if VERBOSE:
+						print(f"{CYN} -- EXECUTING COMMAND --{RST}")
+
+					# EXECUTES COMMAND
+					execution = subprocess.run(argument, capture_output=True, shell=True)
+
+					# INFORM CLIENT OF ANY OUTPUT FILES.
+					output_file = find_output_file(temp_dir, latest_update)
+
+					# SENDING THE OUTPUT BACK TO THE CLIENT.
+					exit_status = send_output(execution, client, temp_dir, output_file)
+					
+					# CHANGE BACK TO ORIGINAL DIRECTORY
+					os.chdir(server_dir)
+
+					# DELETE ANY TEMP DIRECTORIES
+					if temp_dir:
+						print(f"{YEL} - DELETING FILES - {RST}")
+						shutil.rmtree( temp_dir )
+
+					client.close()
+					print(BOLD + ' Client disconnected from sd=' + str(SOCKET_NUM) + '\n' + RST)
+					SOCKET_NUM -= 1
+					print(MAG + "listening on port " + str(PORT_NUM) + ", sd " + str(SOCKET_NUM) + RST)
+					print("----------------------------------------")
+					sys.exit( exit_status )
+
+				# PARENT PROCESS LISTENS FOR OTHER CLIENT REQUEST(S)
+				else:
+					data = None
+					print("created a child process to execute command")
+					break
+					
+			# FINISHED RECEIVING DATA FROM CLIENT
+			else:
+				break
+		
+		client.close()
+		print(BOLD + ' Client disconnected from sd=' + str(SOCKET_NUM) + '\n' + RST)
+		SOCKET_NUM -= 1
+		print(MAG + "listening on port " + str(PORT_NUM) + ", sd " + str(SOCKET_NUM) + RST)
+		print("----------------------------------------")
+
+if __name__ == "__main__":
+	main()
