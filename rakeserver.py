486ff1747c77ba516db36ca14de8761bdb9939f3
AaronAzure <azurakeni@gmail.com>
2022-05-24 14:38:07 +0800

    minimal global vars for server

--- rakeserver.py
+++ rakeserver.py
@@ -10,7 +10,6 @@ import time #! DELETE
 # port number
 HOST = 'localhost'
 PORT_NUM = 12345
-SOCKET_NUM = 0
 VERBOSE = False
 
 BOLD = "\033[1;30m"
@@ -58,7 +57,6 @@ def read_option_flags():
 	global HOST
 	global VERBOSE
 	global PORT_NUM
-	global SOCKET_NUM
 	
 	try:
 		opts, args = getopt.getopt(sys.argv[1:], "vhi:p:")
@@ -87,7 +85,6 @@ def main():
 	global HOST
 	global VERBOSE
 	global PORT_NUM
-	global SOCKET_NUM
 
 	read_option_flags()
 
@@ -97,11 +94,10 @@ def main():
 	# Bind the IP address and the port number
 	sd.bind((HOST, PORT_NUM))
 	
-	SOCKET_NUM = 0
 	# local_ip = socket.gethostbyname("localhost")
 	if VERBOSE:
 		print("IP address = " + HOST)
-	print(MAG + "listening on port=" + str(PORT_NUM) + ", sd=" + str(SOCKET_NUM) + RST)
+	print(MAG + "listening on port=" + str(PORT_NUM) + RST)
 	print("---------------------------------------------")
 
 	# Listen for incoming connections
@@ -110,8 +106,7 @@ def main():
 	# Start accepting client connections
 	while True:
 		client, addr = sd.accept() #! BLOCKING
-		SOCKET_NUM += 1
-		print(BOLD + " Accepted new client on sd=" + str(SOCKET_NUM) + RST)
+		print(BOLD + " Accepted new client" + RST)
 		
 		while True: 
 			data = client.recv(12)     #! BLOCKING
@@ -243,9 +238,8 @@ def main():
 						shutil.rmtree( temp_dir )
 
 					client.close()
-					print(BOLD + ' Client disconnected from sd=' + str(SOCKET_NUM) + '\n' + RST)
-					SOCKET_NUM -= 1
-					print(MAG + "listening on port " + str(PORT_NUM) + ", sd " + str(SOCKET_NUM) + RST)
+					print(BOLD + ' Client disconnected' + '\n' + RST)
+					print(MAG + "listening on port " + str(PORT_NUM) + RST)
 					print("----------------------------------------")
 					sys.exit( exit_status )
 
@@ -260,9 +254,8 @@ def main():
 				break
 		
 		client.close()
-		print(BOLD + ' Client disconnected from sd=' + str(SOCKET_NUM) + '\n' + RST)
-		SOCKET_NUM -= 1
-		print(MAG + "listening on port " + str(PORT_NUM) + ", sd " + str(SOCKET_NUM) + RST)
+		print(BOLD + ' Client disconnected' + '\n' + RST)
+		print(MAG + "listening on port " + str(PORT_NUM) + RST)
 		print("----------------------------------------")
 
 if __name__ == "__main__":
