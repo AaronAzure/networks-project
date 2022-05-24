158ef424ade3a6c096e5d066f82bc61b7de5b123
Xiang <69409366+DLing13@users.noreply.github.com>
2022-05-24 14:47:30 +0800

    Update rakeserver.py

--- MORBIUSWEEP/rakeserver.py
+++ MORBIUSWEEP/rakeserver.py
@@ -8,7 +8,6 @@ import getopt, os, random, tempfile, time
 
 HOST = 'localhost'
 PORT_NUM = 12345
-SOCKET_NUM = 0
 VERBOSE = False
 
 BOLD = "\033[1;30m"
@@ -143,7 +142,6 @@ def read_option_flags():
 	global HOST
 	global VERBOSE
 	global PORT_NUM
-	global SOCKET_NUM
 	
 	try:
 		opts, args = getopt.getopt(sys.argv[1:], "vhi:p:")
@@ -170,7 +168,6 @@ def main():
 	global HOST
 	global VERBOSE
 	global PORT_NUM
-	global SOCKET_NUM
 
 	read_option_flags()
 
@@ -183,7 +180,7 @@ def main():
 	SOCKET_NUM = 0
 	if VERBOSE:
 		print("IP address = " + HOST)
-	print(MAG + "listening on port=" + str(PORT_NUM) + ", sd=" + str(SOCKET_NUM) + RST)
+	print(MAG + "listening on port=" + str(SOCKET_NUM) + RST)
 	print("---------------------------------------------")
 
 	# Listen for incoming connections
@@ -192,8 +189,7 @@ def main():
 	# Start accepting client connections
 	while True:
 		client, addr = sd.accept() #! BLOCKING
-		SOCKET_NUM += 1
-		print(BOLD + " Accepted new client on sd=" + str(SOCKET_NUM) + RST)
+		print(BOLD + " Accepted new client" + RST)
 		
 		while True: 
 			data = client.recv(12)     #! BLOCKING
@@ -267,9 +263,8 @@ def main():
 				break
 		
 		client.close()
-		print(BOLD + ' Client disconnected from sd=' + str(SOCKET_NUM) + '\n' + RST)
-		SOCKET_NUM -= 1
-		print(MAG + "listening on port " + str(PORT_NUM) + ", sd " + str(SOCKET_NUM) + RST)
+		print(BOLD + ' Client disconnected from sd=' + '\n' + RST)
+		print(MAG + "listening on port " + str(PORT_NUM) + RST)
 		print("----------------------------------------")
 
 if __name__ == "__main__":
